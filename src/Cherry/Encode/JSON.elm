054ca8ef49a3276ffcb0e31b0421b06d74faeaed
Andy Thompson <andrew.thompson@qmul.ac.uk>
2021-04-07 18:10:29 +0100

    :sparkles: Add support for encoding Modules as JSON.

--- src/Cherry/Encode/JSON.elm
+++ src/Cherry/Encode/JSON.elm
@@ -1,1 +1,50 @@
 module Cherry.Encode.JSON exposing (..)
+
+
+-- IMPORTS ---------------------------------------------------------------------
+
+
+import Cherry.AST.Module exposing (..)
+import Cherry.Encode.JSON.Declaration as Declaration
+import Cherry.Encode.JSON.Expression as Expression
+import Json.Encode
+import Json.Encode.Extra
+
+
+-- RUNNING THE ENCODER ---------------------------------------------------------
+
+
+encode : Module -> String
+encode module_ =
+    encoder module_
+        |> Json.Encode.encode 4
+
+
+-- ENCODER ---------------------------------------------------------------------
+
+
+encoder : Module -> Json.Encode.Value
+encoder { imports, declarations } =
+    Json.Encode.Extra.taggedObject "Module"
+        [ ( "imports", Json.Encode.list importEncoder imports )
+        , ( "declarations", Json.Encode.list Declaration.encoder declarations )
+        ]
+
+
+-- IMPORT ENCODER --------------------------------------------------------------
+
+
+importEncoder : Import -> Json.Encode.Value
+importEncoder (Import path as_ exposing_) =
+    let
+        asEncoder a =
+            ( "as", Json.Encode.list Json.Encode.string a )
+    
+        exposingEncoder e =
+            ( "exposing", Json.Encode.list Json.Encode.string e )
+    in
+    Json.Encode.Extra.taggedObject "Module.Import" <|
+        ( "path", Json.Encode.string path ) :: List.filterMap identity
+            [ Maybe.map asEncoder as_
+            , Maybe.map exposingEncoder exposing_
+            ]
