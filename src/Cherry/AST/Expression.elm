8a2fb81c20ca75458740e9a71098fb5bf4a9a5f4
Andy Thompson <andrew.thompson@qmul.ac.uk>
2021-04-07 14:34:45 +0100

    :memo: Add some docs to Expression types.

--- src/Cherry/AST/Expression.elm
+++ src/Cherry/AST/Expression.elm
@@ -17,12 +17,55 @@ type Expression
     | Literal Literal
     | Variable Variable
 
-{-| -}
+{-| Accessors encompass the different ways we might acccess the fields or
+properties of an object.
+
+The computed accessor evaluates an expression and uses its result as the key to
+access an object with:
+
+    obj['user-' + 5]
+
+    obj[keyFrom foo]
+
+The fixed accessor accessess an object using a literal key. That key must follow
+the same rules as a valid variable binding (eg starts witha lowercase, can only
+contain alpha-numeric characters):
+
+    obj.user5
+
+    obj.user-5
+
+Note that the second example would subtract 5 from whatever `obj.user` evaluated
+too, unlike the computed accessor that would access the object using the key
+'user-5'.
+-}
 type Accessor
     = Computed Expression
     | Fixed String
 
-{-| -}
+{-| Unlike some other languages, operators are fixed and known at compile-time
+in Cherry. This allows us to perform some optimisations, like removing the pipe
+operator altogether. Here's a list of what symbol(s) each operator is associated
+with and what they do. The JS strict equality operator (===) is used to mark "is
+the same as" and distinguish it from Cherry's equality operator (==). 
+
+    Pipe (|>): Pass a value into a function
+        x |> f === f x
+
+    Compose (>>): Create a new function by calling two functions in sequence
+        f >> g === fun x => g (f x)
+
+    Discard (;): Evaluate the left side, ignore the result, and evalue the right
+        x; y === x |> fun _ => y
+
+    ... You know what maths, comparison, and logic operators look like...
+
+    Cons (::): Prepend a value to an array
+        a :: [ b ] === [ a, b ]
+
+    Join (++): Concatenate two arrays together
+        [ a, b ] ++ [ c, d ] === [ a, b, c, d ]
+-}
 type Operator
     = Pipe | Compose | Discard
     -- MATHS
@@ -49,3 +92,6 @@ type Variable
     | ObjectDestructure (List Variable)
     | Operator Operator
     | Scoped (List String) String
+
+
+-- 
