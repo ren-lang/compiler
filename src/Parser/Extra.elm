ea38c1cc7c8117e483e1672d9136a45fd59b2568
Andy Thompson <andrew.thompson@qmul.ac.uk>
2021-04-09 00:20:38 +0100

    :sparkles: Add functions for stringifying parser deadends.

--- src/Parser/Extra.elm
+++ src/Parser/Extra.elm
@@ -58,3 +58,61 @@ whitespace =
 newlines : Parser ()
 newlines =
     Parser.chompWhile (\c -> c == '\n' || c == '\r')
+
+
+-- DEADENDS --------------------------------------------------------------------
+
+{-| -}
+deadEndsToString : List Parser.DeadEnd -> String
+deadEndsToString deadEnds =
+    List.foldl (++) "" (List.map deadEndToString deadEnds)
+
+{-| -}
+deadEndToString : Parser.DeadEnd -> String
+deadEndToString deadEnd =
+    let
+        position =
+            "row:" ++ String.fromInt deadEnd.row ++ " col:" ++ String.fromInt deadEnd.col ++ "\n"
+    in
+    case deadEnd.problem of
+        Parser.Expecting str ->
+            "Expecting " ++ str ++ " at " ++ position
+
+        Parser.ExpectingInt ->
+            "ExpectingInt at " ++ position
+
+        Parser.ExpectingHex ->
+            "ExpectingHex at " ++ position
+
+        Parser.ExpectingOctal ->
+            "ExpectingOctal at " ++ position
+
+        Parser.ExpectingBinary ->
+            "ExpectingBinary at " ++ position
+
+        Parser.ExpectingFloat ->
+            "ExpectingFloat at " ++ position
+
+        Parser.ExpectingNumber ->
+            "ExpectingNumber at " ++ position
+
+        Parser.ExpectingVariable ->
+            "ExpectingVariable at " ++ position
+
+        Parser.ExpectingSymbol str ->
+            "ExpectingSymbol " ++ str ++ " at " ++ position
+
+        Parser.ExpectingKeyword str ->
+            "ExpectingKeyword " ++ str ++ "at " ++ position
+
+        Parser.ExpectingEnd ->
+            "ExpectingEnd at " ++ position
+
+        Parser.UnexpectedChar ->
+            "UnexpectedChar at " ++ position
+
+        Parser.Problem str ->
+            "ProblemString " ++ str ++ " at " ++ position
+
+        Parser.BadRepeat ->
+            "BadRepeat at " ++ position
