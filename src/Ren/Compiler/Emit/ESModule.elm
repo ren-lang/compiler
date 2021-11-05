ren-lang/compiler

commit 61bef02d2ab547c84ae4e74ff77d9b91bdd06a2a

🔀 ♻️ Remove SubExpression type (#78)
* Remove SubExpression

* Fix tests not compiling, wrap conditionals

* Fix inserting extra parentheses

src/Ren/Compiler/Emit/ESModule.elm
@@ -27,7 +27,8 @@ fromModule { imports, declarations } =
     if List.isEmpty imports then
         declarations
             |> List.map (fromDeclaration >> Pretty.a Pretty.line)
-            |> Pretty.lines 
+            |> Pretty.lines
+
     else
         Pretty.lines (List.map fromImport imports)
             |> Pretty.a Pretty.line
@@ -265,8 +266,46 @@ fromExpression expression =
         Match expr cases ->
             fromMatch expr cases
 
-        SubExpression expr ->
-            fromSubexpression expr
+
+{-| Should contain all expressions which when output either starts or ends with an expression
+-}
+fromExpressionToSingleTerm : Expression -> Pretty.Doc t
+fromExpressionToSingleTerm expression =
+    case expression of
+        Application expr args ->
+            fromApplication expr args
+                |> Pretty.parens
+
+        Conditional condition true false ->
+            fromConditional condition true false
+                |> Pretty.parens
+
+        -- Compiles to Application: always wrap
+        Infix Pipe lhs rhs ->
+            fromInfix Pipe lhs rhs
+                |> Pretty.parens
+
+        -- Compiles to IIFE: never wrap
+        Infix Compose lhs rhs ->
+            fromInfix Pipe lhs rhs
+
+        -- Compiles to []: never wrap
+        Infix Cons lhs rhs ->
+            fromInfix Pipe lhs rhs
+
+        -- Compiles to []: never wrap
+        Infix Join lhs rhs ->
+            fromInfix Pipe lhs rhs
+
+        Infix operator lhs rhs ->
+            fromInfix operator lhs rhs
+
+        Lambda args body ->
+            fromLambda args body
+                |> Pretty.parens
+
+        _ ->
+            fromExpression expression
 
 
 
@@ -275,7 +314,7 @@ fromExpression expression =
 
 fromAccess : Expression -> List Accessor -> Pretty.Doc t
 fromAccess expr accessors =
-    fromExpression expr
+    fromExpressionToSingleTerm expr
         |> Pretty.a (Pretty.join Pretty.empty <| List.map fromAccessor accessors)
 
 
@@ -296,7 +335,7 @@ fromAccessor accessor =
 
 fromApplication : Expression -> List Expression -> Pretty.Doc t
 fromApplication expr args =
-    fromExpression expr
+    fromExpressionToSingleTerm expr
         |> Pretty.a Pretty.space
         |> Pretty.a
             (args
@@ -439,6 +478,10 @@ fromIdentifier identifier =
 
 fromInfix : Operator -> Expression -> Expression -> Pretty.Doc t
 fromInfix operator lhs rhs =
+    let
+        fromInfixOp =
+            fromInfixOperand operator
+    in
     case operator of
         Pipe ->
             fromExpression (Application rhs [ lhs ])
@@ -454,87 +497,164 @@ fromInfix operator lhs rhs =
                 |> Pretty.parens
 
         Add ->
-            fromExpression lhs
+            fromInfixOp lhs
                 |> Pretty.a (Pretty.string " + ")
-                |> Pretty.a (fromExpression rhs)
+                |> Pretty.a (fromInfixOp rhs)
 
         Sub ->
-            fromExpression lhs
+            fromInfixOp lhs
                 |> Pretty.a (Pretty.string " - ")
-                |> Pretty.a (fromExpression rhs)
+                |> Pretty.a (fromInfixOp rhs)
 
         Mul ->
-            fromExpression lhs
+            fromInfixOp lhs
                 |> Pretty.a (Pretty.string " * ")
-                |> Pretty.a (fromExpression rhs)
+                |> Pretty.a (fromInfixOp rhs)
 
         Div ->
-            fromExpression lhs
+            fromInfixOp lhs
                 |> Pretty.a (Pretty.string " / ")
-                |> Pretty.a (fromExpression rhs)
+                |> Pretty.a (fromInfixOp rhs)
 
         Pow ->
-            fromExpression lhs
+            fromInfixOp lhs
                 |> Pretty.a (Pretty.string " ** ")
-                |> Pretty.a (fromExpression rhs)
+                |> Pretty.a (fromInfixOp rhs)
 
         Mod ->
-            fromExpression lhs
+            fromInfixOp lhs
                 |> Pretty.a (Pretty.string " % ")
-                |> Pretty.a (fromExpression rhs)
+                |> Pretty.a (fromInfixOp rhs)
 
         Eq ->
-            fromExpression lhs
+            fromInfixOp lhs
                 |> Pretty.a (Pretty.string " == ")
-                |> Pretty.a (fromExpression rhs)
+                |> Pretty.a (fromInfixOp rhs)
 
         NotEq ->
-            fromExpression lhs
+            fromInfixOp lhs
                 |> Pretty.a (Pretty.string " != ")
-                |> Pretty.a (fromExpression rhs)
+                |> Pretty.a (fromInfixOp rhs)
 
         Lt ->
-            fromExpression lhs
+            fromInfixOp lhs
                 |> Pretty.a (Pretty.string " < ")
-                |> Pretty.a (fromExpression rhs)
+                |> Pretty.a (fromInfixOp rhs)
 
         Lte ->
-            fromExpression lhs
+            fromInfixOp lhs
                 |> Pretty.a (Pretty.string " <= ")
-                |> Pretty.a (fromExpression rhs)
+                |> Pretty.a (fromInfixOp rhs)
 
         Gt ->
-            fromExpression lhs
+            fromInfixOp lhs
                 |> Pretty.a (Pretty.string " > ")
-                |> Pretty.a (fromExpression rhs)
+                |> Pretty.a (fromInfixOp rhs)
 
         Gte ->
-            fromExpression lhs
+            fromInfixOp lhs
                 |> Pretty.a (Pretty.string " >= ")
-                |> Pretty.a (fromExpression rhs)
+                |> Pretty.a (fromInfixOp rhs)
 
         And ->
-            fromExpression lhs
+            fromInfixOp lhs
                 |> Pretty.a (Pretty.string " && ")
-                |> Pretty.a (fromExpression rhs)
+                |> Pretty.a (fromInfixOp rhs)
 
         Or ->
-            fromExpression lhs
+            fromInfixOp lhs
                 |> Pretty.a (Pretty.string " || ")
-                |> Pretty.a (fromExpression rhs)
+                |> Pretty.a (fromInfixOp rhs)
 
         Cons ->
-            fromExpression lhs
+            fromExpressionToSingleTerm lhs
                 |> Pretty.a (Pretty.string ", ...")
-                |> Pretty.a (fromExpression rhs)
+                |> Pretty.a (fromExpressionToSingleTerm rhs)
                 |> Pretty.brackets
 
         Join ->
             Pretty.string "..."
-                |> Pretty.a (fromExpression lhs)
+                |> Pretty.a (fromExpressionToSingleTerm lhs)
                 |> Pretty.a (Pretty.string ", ...")
-                |> Pretty.a (fromExpression rhs)
+                |> Pretty.a (fromExpressionToSingleTerm rhs)
                 |> Pretty.brackets
+
+
+{-| Uses precedence numbers from
+<https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence>
+-}
+esPrecedenceFromInfixOperator : Operator -> Maybe Int
+esPrecedenceFromInfixOperator operator =
+    case operator of
+        Add ->
+            Just 14
+
+        Sub ->
+            Just 14
+
+        Mul ->
+            Just 15
+
+        Div ->
+            Just 15
+
+        Pow ->
+            Just 16
+
+        Mod ->
+            Just 15
+
+        Eq ->
+            Just 11
+
+        NotEq ->
+            Just 11
+
+        Lt ->
+            Just 12
+
+        Lte ->
+            Just 12
+
+        Gt ->
+            Just 12
+
+        Gte ->
+            Just 12
+
+        And ->
+            Just 7
+
+        Or ->
+            Just 6
+
+        _ ->
+            Nothing
+
+
+fromInfixOperand : Operator -> Expression -> Pretty.Doc t
+fromInfixOperand parentOperator operand =
+    let
+        parensRequired =
+            \operator ->
+                case ( esPrecedenceFromInfixOperator parentOperator, esPrecedenceFromInfixOperator operator ) of
+                    ( Just pParent, Just pChild ) ->
+                        pParent > pChild
+
+                    _ ->
+                        False
+    in
+    case operand of
+        Infix operator lhs rhs ->
+            if parensRequired operator then
+                fromInfix operator lhs rhs
+                    |> Pretty.parens
+
+            else
+                fromInfix operator lhs rhs
+
+        _ ->
+            fromExpressionToSingleTerm operand
 
 
 
@@ -1246,16 +1366,6 @@ matchPatternsFromObjectDestructure path patterns =
                         ]
             )
         |> (::) (IsObject path)
-
-
-
--- EMITTING EXPRESSIONS: SUBEXPRESSION -----------------------------------------
-
-
-fromSubexpression : Expression -> Pretty.Doc t
-fromSubexpression expr =
-    fromExpression expr
-        |> Pretty.parens
 
 
 

src/Ren/Compiler/Optimise.elm
@@ -208,7 +208,6 @@ optimiseExpression =
 expressionDefaults : List (Expression -> Maybe Expression)
 expressionDefaults =
     [ constantFold
-    , stripParentheses
     ]
 
 
@@ -258,10 +257,6 @@ transformExpression transform expression =
             Match
                 (transform expr)
                 (List.map (transformCase transform) cases)
-
-        SubExpression expr ->
-            SubExpression
-                (transform expr)
 
 
 transformAccessor : (Expression -> Expression) -> Accessor -> Accessor
@@ -536,110 +531,6 @@ constantFold expression =
         Infix Join (Literal (Array a)) (Literal (Array b)) ->
             Just (a ++ b)
                 |> Maybe.map (Array >> Literal)
-
-        _ ->
-            Nothing
-
-
-stripParentheses : Expression -> Maybe Expression
-stripParentheses expression =
-    case expression of
-        -- SUBEXPRESSION -------------------------------------------------------
-        -- Unwrap all SubExpressions with single expression contents
-        SubExpression (Access expr accessors) ->
-            Access expr accessors
-                |> Just
-
-        SubExpression (Identifier id) ->
-            Identifier id
-                |> Just
-
-        SubExpression (Literal literal) ->
-            Literal literal
-                |> Just
-
-        SubExpression (SubExpression expr) ->
-            SubExpression expr
-                |> Just
-
-        -- ACCESS --------------------------------------------------------------
-        -- Unwrap all SubExpressions inside Computed accessors.
-        -- Removes [(expression)] output.
-        Access expr accessors ->
-            let
-                optimiseAccessor acc =
-                    case acc of
-                        Computed (SubExpression e) ->
-                            Computed e
-
-                        _ ->
-                            acc
-            in
-            List.map optimiseAccessor accessors
-                |> (\optimisedAccessors ->
-                        if optimisedAccessors == accessors then
-                            Nothing
-
-                        else
-                            Access expr optimisedAccessors
-                                |> Just
-                   )
-
-        -- APPLICATION ---------------------------------------------------------
-        -- Unwrap all SubExpressions as arguments.
-        -- Prevents arguments being wrappped in double-parentheses.
-        Application func arguments ->
-            let
-                optimiseArgument arg =
-                    case arg of
-                        SubExpression e ->
-                            e
-
-                        _ ->
-                            arg
-            in
-            List.map optimiseArgument arguments
-                |> (\optimisedArguments ->
-                        if optimisedArguments == arguments then
-                            Nothing
-
-                        else
-                            Application func optimisedArguments
-                                |> Just
-                   )
-
-        -- CONDITIONAL ---------------------------------------------------------
-        -- Unwrap all SubExpressions as condition.
-        -- Prevents condition being wrappped in double-parentheses.
-        Conditional (SubExpression condition) trueCase falseCase ->
-            Conditional condition trueCase falseCase
-                |> Just
-
-        -- Unwrap all SubExpressions as body if true.
-        -- Prevents true body being wrappped in parentheses.
-        Conditional condition (SubExpression trueCase) falseCase ->
-            Conditional condition trueCase falseCase
-                |> Just
-
-        -- Unwrap all SubExpressions as body if false.
-        -- Prevents false body being wrappped in parentheses.
-        Conditional condition trueCase (SubExpression falseCase) ->
-            Conditional condition trueCase falseCase
-                |> Just
-
-        -- LAMBDA --------------------------------------------------------------
-        -- Unwrap SubExpression as body.
-        -- Prevents body being wrappped in parentheses.
-        Lambda patterns (SubExpression body) ->
-            Lambda patterns body
-                |> Just
-
-        -- MATCH ---------------------------------------------------------------
-        -- Unwrap all SubExpressions as arguments.
-        -- Prevents arguments being wrappped in double-parentheses.
-        Match (SubExpression expr) branches ->
-            Match expr branches
-                |> Just
 
         _ ->
             Nothing

src/Ren/Compiler/Parse.elm
@@ -338,7 +338,7 @@ expressionParser =
 {-| -}
 parenthesisedExpressionParser : Pratt.Config Expression -> Parser Expression
 parenthesisedExpressionParser prattConfig =
-    Parser.succeed SubExpression
+    Parser.succeed identity
         |. Parser.symbol "("
         |. Parser.Extra.ignorables
         |= Pratt.subExpression 0 prattConfig

src/Ren/Language.elm
@@ -124,7 +124,6 @@ type Expression
     | Lambda (List Pattern) Expression
     | Literal Literal
     | Match Expression (List ( Pattern, Maybe Expression, Expression ))
-    | SubExpression Expression
 
 
 {-| -}

src/Ren/Language/Expression.elm
@@ -191,9 +191,6 @@ references name expression =
             references name expr
                 || List.any (referencesInCase name) cases
 
-        SubExpression expr ->
-            references name expr
-
 
 referencesInAccessor : String -> Accessor -> Bool
 referencesInAccessor name accessor =
@@ -287,9 +284,6 @@ referencesNamespace namespace expression =
             referencesNamespace namespace expr
                 || List.any (referencesNamespaceInCase namespace) cases
 
-        SubExpression expr ->
-            referencesNamespace namespace expr
-
 
 referencesNamespaceInAccessor : List String -> Accessor -> Bool
 referencesNamespaceInAccessor namespace accessor =
@@ -382,9 +376,6 @@ referencesQualified namespace name expression =
         Match expr cases ->
             referencesQualified namespace name expr
                 || List.any (referencesQualifiedInCase namespace name) cases
-
-        SubExpression expr ->
-            referencesQualified namespace name expr
 
 
 referencesQualifiedInAccessor : List String -> String -> Accessor -> Bool

tests/Parse/Source/Expression.elm
@@ -34,10 +34,8 @@ suite =
         , shouldSucceed Expression.parser
             "(f a) b"
             (Application
-                (SubExpression
-                    (Application (local "f")
-                        [ local "a" ]
-                    )
+                (Application (local "f")
+                    [ local "a" ]
                 )
                 [ local "b" ]
             )
@@ -105,12 +103,10 @@ suite =
             (Infix Sub
                 (Infix Add
                     (Literal (Number 3))
-                    (SubExpression
-                        (Conditional
-                            (Identifier (Local "a"))
-                            (Identifier (Local "b"))
-                            (Identifier (Local "c"))
-                        )
+                    (Conditional
+                        (Identifier (Local "a"))
+                        (Identifier (Local "b"))
+                        (Identifier (Local "c"))
                     )
                 )
                 (Literal (Number 1))

commit 445fa0064c06f42263caa3304a1c20fe7ff7ee46

🐛Fix mistake from removing SubExpressions
Fixes bug introduced in 61bef02.

Copy-paste errors and missing parentheses around top-level infix
expressions (i.e. infix expressions as part of accessed / called
expressions)

src/Ren/Compiler/Emit/ESModule.elm
@@ -287,18 +287,19 @@ fromExpressionToSingleTerm expression =
 
         -- Compiles to IIFE: never wrap
         Infix Compose lhs rhs ->
-            fromInfix Pipe lhs rhs
+            fromInfix Compose lhs rhs
 
         -- Compiles to []: never wrap
         Infix Cons lhs rhs ->
-            fromInfix Pipe lhs rhs
+            fromInfix Cons lhs rhs
 
         -- Compiles to []: never wrap
         Infix Join lhs rhs ->
-            fromInfix Pipe lhs rhs
+            fromInfix Join lhs rhs
 
         Infix operator lhs rhs ->
             fromInfix operator lhs rhs
+                |> Pretty.parens
 
         Lambda args body ->
             fromLambda args body