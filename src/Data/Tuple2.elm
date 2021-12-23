11a80cf6c48cc70001b858197c9ebe14e6118341
Andy Thompson <andrew.thompson@qmul.ac.uk>
2021-12-23 21:14:17 +0000

    :sparkles: Create handy util modules under Data/*.

---
+++ src/Data/Tuple2.elm
@@ -0,0 +1,16 @@
+module Data.Tuple2 exposing (..)
+
+
+from : a -> ( a, a )
+from a =
+    ( a, a )
+
+
+fromBy : (a -> b) -> (a -> c) -> a -> ( b, c )
+fromBy f g a =
+    ( f a, g a )
+
+
+apply : (a -> b -> c) -> ( a, b ) -> c
+apply f ( a, b ) =
+    f a b
