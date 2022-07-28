dfdca29a451e080303f5c03e0b41d2ffdc5919bd
Hayleigh Thompson <me@hayleigh.dev>
2022-07-28 23:45:19 +0100

    :sprkles: Add a span datatype to track source code location of tokens.

---
+++ src/Ren/Data/Span.elm
@@ -0,0 +1,109 @@
+module Ren.Data.Span exposing (..)
+
+{-| -}
+
+-- IMPORTS ---------------------------------------------------------------------
+
+import Json.Decode
+import Json.Encode
+
+
+
+-- TYPES -----------------------------------------------------------------------
+
+
+type alias Span =
+    { start : Pos
+    , end : Pos
+    }
+
+
+type alias Pos =
+    { line : Int
+    , column : Int
+    }
+
+
+
+-- CONSTANTS -------------------------------------------------------------------
+-- CONSTRUCTORS ----------------------------------------------------------------
+
+
+from : ( Int, Int ) -> ( Int, Int ) -> Span
+from ( x, y ) ( a, b ) =
+    { start = Pos x y, end = Pos a b }
+
+
+
+-- QUERIES ---------------------------------------------------------------------
+-- MANIPULATIONS ---------------------------------------------------------------
+
+
+merge : Span -> Span -> Span
+merge a b =
+    let
+        _ =
+            Debug.log "a" a
+
+        _ =
+            Debug.log "b" b
+    in
+    { start =
+        if toComparable a.start < toComparable b.start then
+            a.start
+
+        else
+            b.start
+    , end =
+        if toComparable a.end < toComparable b.end then
+            b.end
+
+        else
+            a.end
+    }
+
+
+
+-- CONVERSIONS -----------------------------------------------------------------
+
+
+toComparable : Pos -> ( Int, Int )
+toComparable { line, column } =
+    ( line, column )
+
+
+
+-- JSON ------------------------------------------------------------------------
+
+
+encode : Span -> Json.Encode.Value
+encode { start, end } =
+    Json.Encode.list Basics.identity
+        [ Json.Encode.list Json.Encode.int
+            [ start.line
+            , start.column
+            ]
+        , Json.Encode.list Json.Encode.int
+            [ end.line
+            , end.column
+            ]
+        ]
+
+
+decoder : Json.Decode.Decoder Span
+decoder =
+    Json.Decode.map2 Span
+        (Json.Decode.index 0 <|
+            Json.Decode.map2 Pos
+                (Json.Decode.index 0 Json.Decode.int)
+                (Json.Decode.index 1 Json.Decode.int)
+        )
+        (Json.Decode.index 1 <|
+            Json.Decode.map2 Pos
+                (Json.Decode.index 0 Json.Decode.int)
+                (Json.Decode.index 1 Json.Decode.int)
+        )
+
+
+
+-- UTILS -----------------------------------------------------------------------
