6c3ee24253bd8e73ee2c349f8df98b3ec372bf28
Hayleigh Thompson <me@hayleigh.dev>
2022-07-28 23:42:20 +0100

    :sparkles: Add dict util functions for updating and combining dictionaries.

--- src/Util/Dict.elm
+++ src/Util/Dict.elm
@@ -10,6 +10,24 @@ import Util.Maybe as Maybe
 -- MODIFICATIONS ---------------------------------------------------------------
 
 
-upsert : comparable -> a -> Dict comparable a -> Dict comparable a
-upsert k v dict =
-    Dict.update k (Maybe.or (Just v)) dict
+upsert : comparable -> a -> (a -> a) -> Dict comparable a -> Dict comparable a
+upsert k v f dict =
+    Dict.update k (Maybe.map f >> Maybe.or (Just v)) dict
+
+
+transform : comparable -> (a -> a) -> Dict comparable a -> Dict comparable a
+transform k f dict =
+    Dict.update k
+        (Maybe.map f)
+        dict
+
+
+combine : (a -> a -> a) -> Dict comparable a -> Dict comparable a -> Dict comparable a
+combine f a b =
+    Dict.merge
+        (\k x d -> Dict.insert k x d)
+        (\k x y d -> Dict.insert k (f x y) d)
+        (\k x d -> Dict.insert k x d)
+        a
+        b
+        Dict.empty
